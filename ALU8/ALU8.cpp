a64d46c82a9226b24f99eed8b310bab012f976a1
Adam <asoliyev13@gmail.com>
2023-02-19 08:50:38 -0600

    added simple multiplication/division ops

--- ALU8/ALU8.cpp
+++ ALU8/ALU8.cpp
@@ -10,31 +10,60 @@ int main(int argc, char **argv) {
 
     alu->A = 0x64;
     alu->B = 0x18;
-    alu->opcode = 0;
 
-    for (int i = 0; i < 10; i++) {
-        alu->eval();
+    alu->opcode = 0; // addition
+    alu->eval();
 
-        // std::cout << std::bitset<8>(alu->A) << std::endl;
-        // std::cout << std::bitset<8>(alu->B) << std::endl;
+    std::cout << "A + B = " << std::bitset<8>(alu->result) << std::endl;
+    if (alu->result != 0x7C) {
+        std::cerr << "Addition test failed!" << std::endl;
+        return 1;
+    }
 
-        std::cout << "A + B = " << std::bitset<8>(alu->result) << std::endl;
-        if (alu->result != (alu->A + alu->B)) {
-            std::cerr << "Test failed!" << std::endl;
-            return 1;
-        }
+    alu->opcode = 1; // subtraction
+    alu->eval();
+    std::cout << "A - B = " << std::bitset<8>(alu->result) << std::endl;
+    if (alu->result != 0x4C) {
+        std::cerr << "Subtraction test failed!" << std::endl;
+        return 1;
+    }
 
-        alu->opcode = 1;
-        alu->eval();
-        std::cout << "A - B = " << std::bitset<8>(alu->result) << std::endl;
-        if (alu->result != (alu->A - alu->B)) {
-            std::cerr << "Test failed!" << std::endl;
-            return 1;
-        }
+    alu->opcode = 2; // multiplication
+    alu->eval();
+    // overflow multiplication
+    std::cout << "A * B = " << std::bitset<8>(alu->result) << std::endl;
+    if (alu->result != 0x60) {
+        std::cerr << "Multiplication test failed!" << std::endl;
+        return 1;
+    }
+    // normal multiplication
+    alu->A = 0x9;
+    alu->B = 0xF;
+    alu->eval();
+    std::cout << "A * B = " << std::bitset<8>(alu->result) << std::endl;
+    if (alu->result != 0x87) {
+        std::cerr << "Multiplication test failed!" << std::endl;
+        return 1;
+    }
 
-        alu->A += 1;
-        alu->B += 2;
-        alu->opcode = 0;
+    alu->opcode = 3; // division
+    alu->A = 0xC0;
+    alu->B = 0x8;
+    alu->eval();
+    // normal division
+    std::cout << "A / B = " << std::bitset<8>(alu->result) << std::endl;
+    if (alu->result != 0x18) {
+        std::cerr << "Division test failed!" << std::endl;
+        return 1;
+    }
+    // division by zero
+    alu->A = 0xC0;
+    alu->B = 0x0;
+    alu->eval();
+    std::cout << "A / B = " << std::bitset<8>(alu->result) << std::endl;
+    if (alu->result != 0xFF) {
+        std::cerr << "Division test failed!" << std::endl;
+        return 1;
     }
 
     std::cout << "All tests passed!" << std::endl;
--- ALU8/ALU8.v
+++ ALU8/ALU8.v
@@ -10,6 +10,25 @@ always @(*) begin
     case (opcode)
         3'b000: result = A + B; // addition
         3'b001: result = A - B; // subtraction
+        3'b010: begin           // multiplication
+            reg [7:0] temp_result = 8'b0;
+            reg [7:0] temp_B = B; 
+            for (int i = 0; i < 8; i = i + 1) begin
+                if (temp_B[0] == 1) begin
+                    temp_result = temp_result + (A << i);
+                end
+                temp_B = temp_B >> 1;
+            end
+            result = temp_result;
+        end
+        3'b011: begin           // division
+            if (B == 0) begin
+                result = 8'hFF;
+            end
+            else begin
+                result = A / B;
+            end
+        end
         default: result = 8'b0; // default to 0
     endcase
 end
