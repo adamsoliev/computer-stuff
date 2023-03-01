f30f0b0d9c10b8710888266d1f2c2b644ee517ef
Adam <asoliyev13@gmail.com>
2023-03-01 16:10:24 -0600

    fetching opcode

--- cpu/cpu.py
+++ cpu/cpu.py
@@ -5,6 +5,8 @@ from elftools.elf.elffile import ELFFile
 import glob
 import binascii
 import subprocess
+from enum import Enum
+import struct
 
 """
     -----------------------
@@ -44,12 +46,12 @@ x7      t2       00111
 x8      s0/fp    01000      Saved register/Frame pointer
 x9      s1       01001      Saved register
 x10     a0       01010      Function arguments/Return values
-x11     a1       01011      
+x11     a1       01011
 x12     a2       01100      Function arguments
 x13     a3       01101
 x14     a4       01110
 x15     a5       01111
-x16     a6       10000      
+x16     a6       10000
 x17     a7       10001
 x18     s2       10010      Saved registers
 x19     s3       10011      (Callee-save registers)
@@ -72,26 +74,44 @@ http://csl.snu.ac.kr/courses/4190.307/2020-1/riscv-user-isa.pdf
 """
 
 """
-RV32I Base Instruction Set - 130 
+RISC-V Instruction-Set
+http://blog.translusion.com/images/posts/RISC-V-cheatsheet-RV32I-4-3.pdf
+
+RV32I Base Instruction Set - 130
 
 Figure 2.2: RISC-V base instruction formats
 ------------------------------------------------------------------------------------------
 31                   25 24    20 19   15 14     12 11                7 6       0
 ------------------------------------------------------------------------------------------
-   funct7              | rs2    | rs1   | funct3  |       rd          |  opcode | R-type
+   funct7              | rs2    | rs1   | funct3  |       rd          | opcode | R-type
+---------------------------------------------------------------------------------------
+imm[11:0]                       | rs1   | funct3  |       rd          | opcode | I-type
+---------------------------------------------------------------------------------------
+imm[11:5]              | rs2    | rs1   | funct3  | imm[4:0]          | opcode | S-type
+---------------------------------------------------------------------------------------
+imm[31:12]                                        |       rd          | opcode | U-type
 ----------------------------------------------------------------------------------------
-imm[11:0]                       | rs1   | funct3  |       rd          |  opcode | I-type
-----------------------------------------------------------------------------------------
-imm[11:5]              | rs2    | rs1   | funct3  | imm[4:0]          |  opcode | S-type
-----------------------------------------------------------------------------------------
-imm[31:12]                                        |       rd          |  opcode | U-type
-----------------------------------------------------------------------------------------
-imm[12] imm[10:5]      | rs2    | rs1   | funct3  | imm[4:1] imm[11]  | opcode  | B-type
-----------------------------------------------------------------------------------------
-imm[20] imm[10:1] imm[11] imm[19:12]              | rd                | opcode  | J-type
+imm[12] imm[10:5]      | rs2    | rs1   | funct3  | imm[4:1] imm[11]  | opcode | B-type
+---------------------------------------------------------------------------------------
+imm[20] imm[10:1] imm[11] imm[19:12]              |       rd          | opcode | J-type
 ------------------------------------------------------------------------------------------
-
 """
+
+
+class Funct7(Enum):
+    ADD = 0b0000000
+    SUB = 0b0100000
+
+
+class Funct3(Enum):
+    ADD = SUB = 0b000
+
+
+class OP(Enum):
+    ADD = SUB = 0b0110011
+    JAL = 0b1101111
+    ECALL = 0b1110011
+
 
 regnames = ["x0", "ra", "sp", "gp", "tp"] + ["t%d" % i for i in range(0, 3)] + ["s0", "s1"] + [
     "a%d" % i for i in range(0, 8)] + ["s%d" % i for i in range(2, 12)] + ["t%d" % i for i in range(3, 7)] + ["PC"]
@@ -133,24 +153,34 @@ def readelf(args):
 
 """
     process execution cycle
-        fetch 
+        fetch
         decode
         execute
+
+1101111 00000 0000000000000000101
+JAL     rd    imm
+              5 (x5/t0)
 """
 
 
 def fetch(addr):
-    print(binascii.hexlify(memory[addr:addr + 4]))
-    """
-    addr - physical address
-    returns instr
-    """
-    return 0
+    assert addr >= 0 and addr < len(memory)
+    # print(memory[addr:addr + 4].hex())
+    return struct.unpack(">I", memory[addr:addr + 4])[0]
 
 
 def execute():
+    global PC
     regfile[PC] -= 0x80000000
-    instr = fetch(regfile[PC])
+    for i in range(20):
+        instr = fetch(regfile[PC])
+        # *** Decode ***
+        op = OP(instr >> 24)
+        print(op)
+
+        # move PC
+        regfile[PC] += 0x4
+
     """
     regfile[PC] points to the next instr
         // fetch next instr
@@ -172,7 +202,7 @@ def tests():
                 if (segment.header.p_paddr < 0x80000000):
                     continue
                 load(segment.header.p_paddr, segment.data())
-                print(segment.header.p_paddr, binascii.hexlify(segment.data()))
+                # print(segment.header.p_paddr, binascii.hexlify(segment.data()))
             regfile[PC] = 0x80000000
             instrcnt = 0
             while execute():
